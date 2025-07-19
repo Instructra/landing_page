8119dcd24a85d206b053b312511101d0b7aff927
AbdulbasitSaid <mrspeed518@gmail.com>
2025-07-19 10:42:34 +0100

    Refactor Header component for improved structure and responsiveness; add dynamic header height management

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,25 +1,43 @@
+"use client";
+
 import Link from "next/link";
+import { useRef, useEffect } from "react";
+import { useYieldContext } from "~/contexts/YieldContext";
 
 export default function Header() {
+  const ref = useRef<HTMLDivElement>(null);
+  const { setHeaderHeight } = useYieldContext();
+
+  useEffect(() => {
+    const updateHeight = () => {
+      if (ref.current) {
+        setHeaderHeight(ref.current.getBoundingClientRect().height);
+      }
+    };
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+    return () => window.removeEventListener("resize", updateHeight);
+  }, [setHeaderHeight]);
   return (
-    <div className="fixed flex w-screen justify-center bg-(--bg-white) py-[32px]">
-      <nav className="card-bg ll:w-[1200px] tb:w-[704px] flex w-[300px] items-center justify-between rounded-[100px] px-4 py-3">
+    <div
+      ref={ref}
+      className="fixed z-10 flex w-screen justify-center bg-white py-[32px]"
+    >
+      <nav className="card-bg l:w-(--max-l) ll:w-(--max-ll) tb:w-(--max-tb) mm:w-(--max-mm) ml:w-(--max-ml) flex w-(--max-sm) items-center justify-between rounded-[100px] px-4 py-3">
         <Link href={"/#home"}>
-          <div className="text-primary bg-amber-600 px-4 py-3 font-bold">
-            INSTRUCTRA
-          </div>
+          <div className="text-primary px-4 py-3 font-bold">INSTRUCTRA</div>
         </Link>
-        <div className="ll:flex hidden gap-4">
+        <div className="l:flex hidden gap-4">
           <Link href={"/#home"}>Home</Link>
           <Link href={"/#about"}>About</Link>
           <Link href={"/#how"}>How it works</Link>
           <Link href={"/#contact"}>Contact us</Link>
           <Link href={"/referral"}>Referral programme</Link>
         </div>
-        <button className="ll:inline bg-primary hidden rounded-4xl px-6 py-3 text-white">
+        <button className="l:inline bg-primary hidden rounded-4xl px-6 py-3 text-white">
           Join the wait list
         </button>
-        <button className="ll:hidden">🥞</button>
+        <button className="l:hidden">🥞</button>
       </nav>
     </div>
   );
