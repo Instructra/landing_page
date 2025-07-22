b3e25d094fdd73961b2e6254a3a81627810bec71
AbdulbasitSaid <mrspeed518@gmail.com>
2025-07-23 00:48:57 +0100

    refactor reusable button

--- src/app/_components/Buttons.tsx
+++ src/app/_components/Buttons.tsx
@@ -1,24 +1,28 @@
+export enum ButtonType {
+  Primary = "PRIMARY",
+  Secondary = "SECONDARY",
+  Other = "OTHER",
+}
+
 export interface IButtonProps {
   text: string | null;
   buttonType: ButtonType;
 }
-export enum ButtonType {
-  Primary = "PRIMARY",
-  Secondary = "SECONDARY",
-  Other = "Other",
-}
-export function Buttons(props: IButtonProps) {
-  return (
-    <button
-      className={`${
-        props.buttonType == ButtonType.Primary
-          ? "bg-primary text-white"
-          : props.buttonType == ButtonType.Secondary
-            ? "text-text-primary border-border border-2 bg-white"
-            : ""
-      } rounded-full px-6 py-3`}
-    >
-      {props.text}
-    </button>
-  );
+
+export function Buttons({ text, buttonType }: IButtonProps) {
+  const baseClasses = "rounded-full px-6 py-3";
+  let variantClasses = "";
+
+  switch (buttonType) {
+    case ButtonType.Primary:
+      variantClasses = "bg-primary text-white";
+      break;
+    case ButtonType.Secondary:
+      variantClasses = "text-text-primary border-border border-2 bg-white";
+      break;
+    default:
+      variantClasses = "";
+  }
+
+  return <button className={`${baseClasses} ${variantClasses}`}>{text}</button>;
 }
