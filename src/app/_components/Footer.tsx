ba9e1172480a3c8f5db81e88e1f21f0ca72a2d96
AbdulbasitSaid <mrspeed518@gmail.com>
2025-07-31 08:24:38 +0100

    created footer component

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,11 @@
+import MainContainer from "./MainContainer";
+
+export default function Footer() {
+  return (
+    <section id="contact" className="flex  justify-center items-start bg-dark-bg">
+      <MainContainer>
+        <h2 className="text-start text-4xl font-semibold text-white ">Let’s talk</h2>
+      </MainContainer>
+    </section>
+  );
+}
