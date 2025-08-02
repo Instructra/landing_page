f30e5f6e325fa8f57323f1062edf53b90182a145
AbdulbasitSaid <mrspeed518@gmail.com>
2025-08-02 19:40:42 +0100

    completed video player

--- src/app/_components/VideoPlayer.tsx
+++ src/app/_components/VideoPlayer.tsx
@@ -1,33 +1,30 @@
-import { Suspense } from "react";
+"use client";
+
+import { useYieldContext } from "~/contexts/YieldContext";
 
 export default function VideoPlayer() {
+  const { videoUrl, closeVideo } = useYieldContext();
+
+  if (!videoUrl) return null;
+
   return (
-    <div className="fixed top-0 right-0 bottom-0 left-0 z-60 h-dvh w-dvw bg-white/5 backdrop-blur-xs">
-      <div className="fixed top-1/2 left-1/2 z-70 flex h-4/7 w-4/5 -translate-x-1/2 -translate-y-1/2">
-        <Suspense fallback={VideoLoader()}>
-          <div className="relative h-full w-full rounded-4xl bg-black/80">
-            <video className="bg-primary/20 relative aspect-3/2 h-full w-full rounded-4xl object-contain backdrop-blur-3xl">
-              <source
-                src="https://hncbsyyhcafzhkqlolkd.supabase.co/storage/v1/object/public/landingpage//how_instructra_works.mp4"
-                type="video/mp4"
-              />
-              Your browser does not support the video tag.
-              {/* play */}
-            </video>
-            <div className="absolute top-1/2 left-1/2 flex h-16 w-16 -translate-x-1/2 -translate-y-1/2 items-center justify-center">
-              {/* play icon */}
-              <span className="icon-[solar--play-bold] bg-white text-6xl"></span>
-            </div>
-          </div>
-        </Suspense>
-      </div>
-      <div className="absolute bottom-16 left-1/2 flex h-12 w-12 -translate-x-1/2 items-center justify-center rounded-full bg-[#1f1f1f]">
-        <span className="icon-[material-symbols--close] text-4xl text-white"></span>
+    <div className="fixed inset-0 z-70 flex items-center justify-center bg-white/10 backdrop-blur-sm">
+      <div className="relative w-11/12 max-w-4xl shadow-lg">
+        <video
+          controls
+          className="h-auto w-full rounded-2xl"
+          src={videoUrl}
+          autoPlay
+        >
+          Your browser does not support the video tag.
+        </video>
+        <button
+          onClick={closeVideo}
+          className="absolute right-1/2 -bottom-16 flex h-10 w-10 translate-x-1/2 items-center justify-center rounded-full bg-[#1F1F1F] shadow-md"
+        >
+          <span className="icon-[material-symbols--close] text-2xl text-white"></span>{" "}
+        </button>
       </div>
     </div>
   );
-}
-
-function VideoLoader() {
-  return <div className="h-4/5 w-4/5 bg-gray-400"></div>;
 }
