2c0091429babc2a8c37e08fb50c548f52759e840
AbdulbasitSaid <mrspeed518@gmail.com>
2025-08-16 09:05:23 +0100

    feat: enhance WaitListDialog with user type selection and add WaitListForm component for email submission

--- src/app/_components/WaitListDialog.tsx
+++ src/app/_components/WaitListDialog.tsx
@@ -1,10 +1,18 @@
 "use client";
-import { useWaitListStore } from "~/store/WaitListStore";
+import {
+  SelectedUserType,
+  useWaitListStore,
+  WaitListStages,
+  type WaitListStore,
+} from "~/store/WaitListStore";
+
+import { WaitListForm } from "./WaitListForm";
 
 export function WaitListDialog() {
-  const waitListDialogState = useWaitListStore((state) => state);
+  const waitListDialogStore = useWaitListStore((state) => state);
+  const waitListState = waitListDialogStore.state;
 
-  if (waitListDialogState.state.isActive === false) {
+  if (waitListState.isActive === false) {
     return null;
   }
   return (
@@ -13,33 +21,80 @@ export function WaitListDialog() {
         {/* header */}
         <div className="">
           <div className="flex justify-between">
-            <h2 className="text-2xl">Join the waitlist</h2>
-            <button onClick={waitListDialogState.toggleDialog}>
+            {waitListState.dialogStage == WaitListStages.SELECTION && (
+              <h2 className="text-2xl">Join the wait list</h2>
+            )}
+            {waitListState.dialogStage == WaitListStages.EMAIL_COLLECTION && (
+              <div className="text-primary font-bold">INSTRUCTRA</div>
+            )}
+            <button onClick={waitListDialogStore.toggleDialog}>
               <span className="icon-[material-symbols--close] text-2xl"></span>
             </button>
-          </div>
-          <p className="text-sm">Please select an option to continue</p>
+          </div>{" "}
+          {waitListState.dialogStage == WaitListStages.SELECTION && (
+            <p className="text-sm">Please select an option to continue</p>
+          )}
         </div>
         {/* body */}
-        <div className="flex w-full flex-wrap justify-center gap-4">
-          {/* Instructor */}
-          <div className="tb:w-[312px] hover:border-primary flex w-full flex-col gap-8 rounded-3xl bg-white px-3 py-6 transition-all duration-300 ease-in-out hover:border-2">
-            <div className="tb:h-[160px] h-[80] w-full bg-amber-200"></div>
-            <div>
-              <p className="text-xl">Join as an Instructor</p>
-              <p>Join the waitlist as an instructor</p>
-            </div>
-          </div>
-          {/* Learner */}
-          <div className="tb:w-[312px] hover:border-primary flex w-full flex-col gap-8 rounded-3xl bg-white px-3 py-6 transition-all duration-300 ease-in-out hover:border-2">
-            <div className="tb:h-[160px] h-[80] w-full bg-amber-200"></div>
-            <div>
-              <p className="text-xl">Join as an Learner</p>
-              <p>Join the waitlist as an Learner</p>
-            </div>
-          </div>
+        {waitListState.dialogStage == WaitListStages.SELECTION
+          ? UserTypeSelector(waitListDialogStore)
+          : SubmissionForm(waitListDialogStore)}
+      </div>
+    </div>
+  );
+}
+function UserTypeSelector(waitListDialogStore: WaitListStore) {
+  return (
+    <div className="flex w-full flex-wrap justify-center gap-4">
+      {/* Instructor */}
+      <div
+        className="tb:w-[312px] hover:border-primary flex w-full flex-col gap-8 rounded-3xl bg-white px-3 py-6 transition-all duration-300 ease-in-out hover:border-2"
+        onClick={() =>
+          waitListDialogStore.nextStage(SelectedUserType.INSTRUCTOR)
+        }
+      >
+        <div className="tb:h-[160px] h-[80] w-full bg-amber-200"></div>
+        <div>
+          <p className="text-xl">Join as an Instructor</p>
+          <p>Join the waitlist as an instructor</p>
         </div>
       </div>
+      {/* Learner */}
+      <div
+        className="tb:w-[312px] hover:border-primary flex w-full flex-col gap-8 rounded-3xl bg-white px-3 py-6 transition-all duration-300 ease-in-out hover:border-2"
+        onClick={() => waitListDialogStore.nextStage(SelectedUserType.LEARNER)}
+      >
+        <div className="tb:h-[160px] h-[80] w-full bg-amber-200"></div>
+        <div>
+          <p className="text-xl">Join as an Learner</p>
+          <p>Join the waitlist as an Learner</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SubmissionForm(waitListDialogStore: WaitListStore) {
+  return (
+    <div className="flex flex-col items-center gap-8">
+      <h2 className="max-w-[246px] text-center text-[32px] font-[500]">
+        {waitListDialogStore.state.selectedUserType ===
+        SelectedUserType.INSTRUCTOR
+          ? "Join the WaitList as an Instructor"
+          : "Join the WaitList as a Learner"}
+      </h2>
+      {/* list of icons */}
+      <div className="l:h-9 l:w-[180] relative flex h-12 w-[240px] justify-center">
+        <div className="l:w-9 l:h-9 absolute left-0 h-12 w-12 rounded-full bg-[url('/assets/images/wait_1.jpg')] bg-cover"></div>
+        <div className="l:w-9 l:h-9 l:left-[32] absolute left-[44] h-12 w-12 rounded-full bg-[url('/assets/images/wait_2.jpg')] bg-cover"></div>
+        <div className="l:w-9 l:h-9 l:left-[64] absolute left-[88] h-12 w-12 rounded-full bg-[url('/assets/images/wait_3.jpg')] bg-cover"></div>
+        <div className="l:w-9 l:h-9 l:left-[96] absolute left-[132] h-12 w-12 rounded-full bg-[url('/assets/images/wait_4.jpg')] bg-cover"></div>
+        <div className="l:w-9 l:h-9 l:left-[128] absolute left-[176] h-12 w-12 rounded-full bg-[url('/assets/images/wait_5.jpg')] bg-cover"></div>
+      </div>
+
+      <WaitListForm />
+
+      <p>Enter your email above to join our early access.</p>
     </div>
   );
 }
