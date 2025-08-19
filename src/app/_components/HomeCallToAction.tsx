2b550d0cb56126e8905fae0ba82a904463ef1e4a
AbdulbasitSaid <mrspeed518@gmail.com>
2025-08-19 09:51:33 +0100

    fixed failing how it works

--- src/app/_components/HomeCallToAction.tsx
+++ src/app/_components/HomeCallToAction.tsx
@@ -1,13 +1,13 @@
 "use client";
 
+import HowItWorksCallToAction from "./HowItWorksCallToAction";
 import { JoinWaitListButton } from "./JoinWaitListButton";
 
 export default function HomeCallToAction() {
   return (
     <div className="tb:flex-row tb:justify-center tb:items-center tb:row-start-3 l:row-start-4 l:row-span-1 l:col-start-1 l:col-span-3 l:justify-start tb:row-span-1 row-span-1 flex flex-col gap-4">
       <JoinWaitListButton />
-
-      <HomeCallToAction />
+      <HowItWorksCallToAction />
     </div>
   );
 }
