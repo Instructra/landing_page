345f69d69fb30474cd6a7a0278161c489c28b176
AbdulbasitSaid <mrspeed518@gmail.com>
2025-08-23 02:06:37 +0100

    fix hidden join wait list button

--- src/app/_components/HomeCallToAction.tsx
+++ src/app/_components/HomeCallToAction.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { useWaitListStore } from "~/store/WaitListStore";
 import HowItWorksCallToAction from "./HowItWorksCallToAction";
-import { JoinWaitListButton } from "./JoinWaitListButton";
 
 export default function HomeCallToAction() {
+  const toggleDialog = useWaitListStore((state) => state).toggleDialog;
+
   return (
     <div className="tb:flex-row tb:justify-center tb:items-center tb:row-start-3 l:row-start-4 l:row-span-1 l:col-start-1 l:col-span-3 l:justify-start tb:row-span-1 row-span-1 flex flex-col gap-4">
-      <JoinWaitListButton />
+      <button
+        className="bg-primary rounded-4xl px-6 py-3 text-white"
+        onClick={toggleDialog}
+      >
+        Join the wait list
+      </button>
       <HowItWorksCallToAction />
     </div>
   );
