1780d2719cecebd03a9f38645c67aafc1851efd4
AbdulbasitSaid <mrspeed518@gmail.com>
2025-08-26 15:38:00 +0100

    removed the 1,2,3 on "who is instructra for"

--- src/app/_components/Slider.tsx
+++ src/app/_components/Slider.tsx
@@ -21,26 +21,17 @@ export default function Slider() {
         {/* Instructor */}
         <div className="tb:min-w-[300px] l:min-w-[336px] l:h-[348px] ll:min-w-[390px] ll:h-[450px] tb:h-[348px] flex h-64 min-w-[220px] items-end justify-center gap-1 rounded-2xl bg-[url('/assets/images/instructor.jpg')] bg-cover bg-center pb-6 transition-transform duration-300">
           <p className="rounded-full bg-white px-4 py-2 text-[16px]/6 font-normal">
-            1
-          </p>
-          <p className="rounded-full bg-white px-4 py-2 text-[16px]/6 font-normal">
             Instructors
           </p>
         </div>
         {/* Leaner */}
         <div className="tb:min-w-[300px] l:min-w-[336px] l:h-[348px] ll:min-w-[390px] ll:h-[450px] tb:h-[348px] flex h-64 min-w-[220px] items-end justify-center gap-1 rounded-2xl bg-[url('/assets/images/learner.jpg')] bg-cover bg-center pb-6 transition-transform duration-300">
           <p className="rounded-full bg-white px-4 py-2 text-[16px]/6 font-normal">
-            2
-          </p>
-          <p className="rounded-full bg-white px-4 py-2 text-[16px]/6 font-normal">
             Learners
           </p>
         </div>
         {/* Driving school */}
         <div className="tb:min-w-[300px] l:min-w-[336px] l:h-[348px] ll:min-w-[390px] ll:h-[450px] tb:h-[348px] flex h-64 min-w-[220px] items-end justify-center gap-1 rounded-2xl bg-[url('/assets/images/driving_school.jpg')] bg-cover bg-center pb-6 transition-transform duration-300">
-          <p className="rounded-full bg-white px-4 py-2 text-[16px]/6 font-normal">
-            3
-          </p>
           <p className="rounded-full bg-white px-4 py-2 text-[16px]/6 font-normal">
             Driving Schools
           </p>
